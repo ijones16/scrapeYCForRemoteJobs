63ffc75d64e385b510a1241178bfd71f1d1cb0f0
ijones16 <ian@egghead.io>
2016-05-15 23:31:06 -0700

    script prints results

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var Xray = require('x-ray');
+var read = require('fs').readFileSync;
 
 var xray = new Xray();
 
@@ -8,4 +9,15 @@ xray('https://news.ycombinator.com/jobs', '.athing',
         link: 'td:nth-child(3) a@href'
     }]
     )
-        .write('./results.json');
+        .write('./results.json')
+        .on('finish', function(){
+            var jobs = JSON.parse(read('./results.json', 'utf8'))
+            var remoteJobs = jobs.map(function(job){
+                if (job.title.includes('remote') || job.title.includes('Remote')){
+                    return job;
+                }
+                return;
+            })
+
+            console.log(remoteJobs);
+        });
