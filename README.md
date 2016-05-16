350b81ef888f79725579a5f9996020b7a9bc2b63
ijones16 <ian@egghead.io>
2016-05-15 23:42:25 -0700

    improve script

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 ## This will return the first page of jobs.
 - assuming you have node installed:
 - node script.js
-- node filterResults.js
 
--- script.js
+++ script.js
@@ -9,15 +9,17 @@ xray('https://news.ycombinator.com/jobs', '.athing',
         link: 'td:nth-child(3) a@href'
     }]
     )
+        .paginate('a[rel="nofollow"]:last-child@href')
+        .limit(10)
         .write('./results.json')
         .on('finish', function(){
             var jobs = JSON.parse(read('./results.json', 'utf8'))
-            var remoteJobs = jobs.map(function(job){
-                if (job.title.includes('remote') || job.title.includes('Remote')){
-                    return job;
-                }
-                return;
+            var remoteJobs = jobs.filter(function(job){
+                return job.title.includes('remote') || job.title.includes('Remote')
             })
-
             console.log(remoteJobs);
+            console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n end of script");
         });
+        console.log('retrieving jobs...')
+        console.log('this may take a couple seconds.')
+ 
